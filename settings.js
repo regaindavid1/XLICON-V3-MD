c657d8b6c39df8d1a81fb9e1433ca4b91b954196
regaindavid1 <ochijeregain2007@gmail.com>
2024-05-15 12:45:06 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -52,9 +52,9 @@ global.location = process.env.GL_INFO || "Pakistan, Lahore, Sabzazar" //ur locat
 //________________________________________________________________________________________________________
 global.botname = process.env.BOT_NAME || 'XLICON-V3-MD' //ur bot name
 //________________________________________________________________________________________________________
-global.ownernumber = process.env.SUDO || '923184070915' //ur owner number
+global.ownernumber = process.env.SUDO || '+2347039738922' //ur owner number
 //________________________________________________________________________________________________________
-global.ownername = process.env.SUDO_NAME || 'Salman Ahmad' //ur owner name
+global.ownername = process.env.SUDO_NAME || 'REGAINDAVID' //ur owner name
 //________________________________________________________________________________________________________
 global.websitex = process.env.YT_LINK || "https://youtube.com/@s4salmanyt"
 //________________________________________________________________________________________________________
@@ -99,7 +99,7 @@ global.adminevent = process.env.ANNOUNCE_MSG  || true //show promote/demote mess
 //________________________________________________________________________________________________________
 global.groupevent = process.env.GROUP_ANNOUNCE_MSG || true //show update messages in group chat
 //________________________________________________________________________________________________________
-
+global.antilink = process.env.AUTO_DELETE_WARN_MSG || true //auto delete
 
 //msg
 global.mess = {
